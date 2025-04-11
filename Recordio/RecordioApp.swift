8f97fa5b8b172c3ea43fc9c61f4693040a4b8a44
Jess Nelson <jemane.19@gmail.com>
2025-04-10 22:12:31 -0600

    Initial Commit

---
+++ Recordio/RecordioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordioApp.swift
+//  Recordio
+//
+//  Created by Jesssica Nelson on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RecordioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
